fe413f5b70776fdb93df3be8c7375895bb58c253
johantentij <johantentij@gmail.com>
2026-01-22 00:24:11 +0100

    ant colony verbtered

--- main.py
+++ main.py
@@ -1,167 +1,99 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import networkx
-
-class Edge:
-    def __init__(self, start, end, cost=np.random.rand() + .1):
-        self.start = start
-        self.end = end
-
-        self.cost = cost
-        self.pheromone = 0
-        self.deposited = 0
-
-        return
+import networkx as nx
     
-    def update(self):
-        self.pheromone = (1 - evaporation) * self.pheromone + self.deposited
-        self.deposited = 0
+N_network = 100
+p = .0
 
-        return
-    
-N_network = 50
-p = .2
+G = nx.random_geometric_graph(500, 0.2)
 
-G = networkx.erdos_renyi_graph(N_network, p)
+for u, v in G.edges:
+    G[u][v]["cost"] = np.random.rand() + 0.1
+    G[u][v]["pheromone"] = 1.0
 
-networkx.draw(G)
-plt.show()
+def build_path(G, start, end, alpha, beta):
+    path = []
+    current = start
+    visited = {start}
 
-edges = [Edge(x[0], x[1]) for x in G.edges()]
+    while current != end:
+        neighbors = [
+            v for v in G.neighbors(current)
+            if v not in visited
+        ]
+        if not neighbors:
+            return None  # dead end
 
-edgesStart = [[] for _ in range(N_network)]
-for edge in edges:
-    edgesStart[edge.start].append(edge)
-    edgesStart[edge.end].append(edge)
-    
+        probs = []
+        for v in neighbors:
+            edge = G[current][v]
+            p = (edge["pheromone"] ** alpha) * ((1 / edge["cost"]) ** beta)
+            probs.append(p)
 
-class Ant:
-    def __init__(self, start, dest, alpha, beta):
-        self.start = start
-        self.pos = start
-        self.dest = dest
+        probs = np.array(probs)
+        probs /= probs.sum()
 
-        self.alpha = alpha
-        self.beta = beta
+        next_node = np.random.choice(neighbors, p=probs)
+        path.append((current, next_node))
+        visited.add(next_node)
+        current = next_node
 
-        self.path = []
-        self.pathLen = 0
-        self.arrived = False
+    return path
 
-        return
+def update_pheromones(G, paths, Q, evaporation):
+    # evaporation
+    for u, v in G.edges:
+        G[u][v]["pheromone"] *= (1 - evaporation)
 
-    def p(self, edge):
-        return edge.pheromone ** self.alpha * edge.cost ** self.beta
+    # deposit
+    for path, length in paths:
+        deposit = Q / length
+        for u, v in path:
+            G[u][v]["pheromone"] += deposit
 
-    def step(self):
-        if self.arrived:
-            return
-        
-        possibilities = edgesStart[self.pos]
-
-        probabilities = []
-        for i in range(len(possibilities)):
-            probabilities.append(self.p(possibilities[i]))
-
-        totP = sum(probabilities)
-        if totP:
-            for i in range(len(probabilities)):
-                probabilities[i] /= totP
-
-            chosenEdge = np.random.choice(possibilities, p=probabilities)
-
-        # edge case: when no pheromone yet -> all p are zero
-        else:
-            chosenEdge = np.random.choice(possibilities)
-
-        if chosenEdge.start == self.pos:
-            self.pos = chosenEdge.end
-        else:
-            self.pos = chosenEdge.start
-
-        self.path.append(chosenEdge)
-
-        if self.pos == self.dest:
-            self.arrived = True
-
-        return
-    
-    def update(self):
-        self.pathLen = 0
-        for edge in self.path:
-            self.pathLen += edge.cost
-
-        for edge in self.path:
-            edge.deposited += Q / self.pathLen
-
-        self.path = []
-        self.arrived = False
-        self.pos = self.start
-
-        return
-
-N_ants = 20
-maxIterAnt = int(1e4)
-maxIterColony = int(1e2)
-
-threshold = .8
-threshold = int(threshold * N_ants)
-
+N_ANTS = 10
+ITER = 100
+alpha = 1.5
+beta = 1.5
 Q = 10
-alpha = 2
-beta = -1.5
-evaporation = .3
+evaporation = 0.5
 
-startEdge, endEdge = np.random.choice(edges, 2, replace=False)
-start = startEdge.start
-end = endEdge.end
+start = np.random.choice(G.nodes)
+path = [start]
+pos = start
 
-ants = [Ant(start=start, dest=end, alpha=alpha, beta=beta) for _ in range(N_ants)]
-
-for i in range(maxIterColony):
-    for j in range(maxIterAnt):
-        done = True
-        for ant in ants:
-            ant.step() 
-            done = done and ant.arrived
-
-        if done:
-            break
-    
-    for ant in ants:
-        ant.update()
-
-    pathLens = [ant.pathLen for ant in ants]
-    pathLens.sort()
-
-    j = 0
-    while j < N_ants and pathLens[j] == pathLens[0]:
-        j += 1
-    
-    if (j >= threshold):
-        print("total iterations: %d" % i)
+for i in range(50):
+    neighbors = [
+        v for v in G.neighbors(pos)
+        if v not in path
+    ]
+    if not neighbors:
         break
 
-    for edge in edges:
-        edge.update()
+    path.append(np.random.choice(neighbors))
 
+end = path[-1]
 
-for edge in edges:
-    print(edge.pheromone)
+solutions = []
+for it in range(ITER):
+    solutions = []
 
+    for _ in range(N_ANTS):
+        path = build_path(G, start, end, alpha, beta)
+        if path:
+            length = sum(G[u][v]["cost"] for u, v in path)
+            solutions.append((path, length))
 
-# notes
+    if not solutions:
+        continue
 
-# make graph directed, also higher p
-# make random path for initial condition
-# fix decision function
-# reset ant position
+    update_pheromones(G, solutions, Q, evaporation)
 
+    best = min(length for _, length in solutions)
+    print(f"iter {it}, best length {best:.3f}")
 
-# optimisations
+print(min(solutions, key= lambda x : x[1]))
 
-# dont store entire path -> extrapolate from pheromones
-# precompute decision function for edges -> in Edge.update()
 
 
 
